575c62a1c6a56768a351d8a3d0ab89a028cf138c
Unknown <villajohnny@gmail.com>
2019-04-23 01:43:26 -0500

    Commit Final
    Juego basico funcional

---
+++ Assets/Scripts/jugadorController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class jugadorController : MonoBehaviour
+{
+    Rigidbody rb;
+    public Text puntuacion;
+    public Text win;
+    public int contador;
+    public float velocidad;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        contador = 0;
+        ActualizarMarcador();
+        win.gameObject.SetActive(false);
+    }
+    public void FixedUpdate()
+    {
+        float movimientoHorizontal= Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+        Vector3 movimiento = new Vector3(movimientoHorizontal,0, movimientoVertical);
+        rb.AddForce(movimiento*velocidad);
+
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Destroy(other.gameObject);
+        contador++;
+        Debug.Log(contador);
+        ActualizarMarcador();
+
+        if (contador >= 2)
+        {
+            win.gameObject.SetActive(true);
+        }
+    }
+
+    private void ActualizarMarcador()
+    {
+        puntuacion.text = "Puntuacion: " + contador;
+    }
+}
---
+++ Assets/Scripts/pointTurnController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointTurnController : MonoBehaviour
+{
+    Vector3 rotacion = new Vector3(15,30,45);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotacion*Time.deltaTime);
+    }
+
+}
